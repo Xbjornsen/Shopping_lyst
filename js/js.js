3854698568a5cb2438f57b574b68b8ab08721563
Reeahn <ryan.toeta@gmail.com>
2018-08-31 15:50:58 +0930

    poohead

--- js/js.js
+++ js/js.js
@@ -1,5 +1,14 @@
 var conn = indexedDB.open('shopping_list', 1);
 
+fucntion loadData(db) {
+
+}
+function bindSaveButton(db) {
+  var shoppingItem = document.getElementById('');
+  var saveButton = document.getElementById('add')
+  )
+}
+
 conn.addEventListener('success', function(evt) {
   var db = evt.target.result;
   console.log('connected event', evt);
@@ -14,4 +23,24 @@ conn.addEventListener('error', function(evt) {
 
 conn.addEventListener('upgradeneeded',function(evt) {
   console.log('upgrade needed', evt)
+});
+
+conn.addEventListener('upgradeneeded',function(evt) {
+  console.log('upgrade needed', evt)
+  var db = evt.target.result;
+
+  // Create an objectStore for this database
+  var objectStore = db.createObjectStore("listItems");
+  objectStore.createIndex(r
+    'item',
+    'item',
+    {unique: false}
+  );
+
+  // Listen for the completed transaction
+  objectStore
+    .transaction
+    .addEventListener('complete', function(evt) {
+   console.log('Store created');
+  });
 });
