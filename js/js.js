68cc4a07895eb071a2b208bf6a57bf3536be5677
Reeahn <ryan.toeta@gmail.com>
2018-08-31 16:12:39 +0930

    poop

--- js/js.js
+++ js/js.js
@@ -1,17 +1,40 @@
 var conn = indexedDB.open('shopping_list', 1);
 
-fucntion loadData(db) {
+function createRow(db) {
+  var row = document.createElement('tr');
+  row.classicList.add('itemRow');
+  var nameCell = document.createElement('td');
+  nameCell.innerHTML = data.item;
+  row.appendChild(nameCell);
+  return row;
+}
+
+function loadData(db) {
 
 }
 function bindSaveButton(db) {
-  var shoppingItem = document.getElementById('');
-  var saveButton = document.getElementById('add')
-  )
+  var shoppingItem = document.getElementById('ItemInput');
+  var saveButton = document.getElementById('add');
+  saveButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    var transaction = db.transaction(['listItems'],'readwrite');
+    var objectStore = transaction.objectStore('listItems');
+    var request = objectStore.add({item: shoppingItem});
+    request.addEventListener('success', function(evt) {
+      console.log(
+        'Successfully added data',
+        evt.target.result
+      );
+    });
+  } );
 }
+
+
 
 conn.addEventListener('success', function(evt) {
   var db = evt.target.result;
   console.log('connected event', evt);
+  bindSaveButton(db);
 });
 
 conn.addEventListener('error', function(evt) {
@@ -30,12 +53,8 @@ conn.addEventListener('upgradeneeded',function(evt) {
   var db = evt.target.result;
 
   // Create an objectStore for this database
-  var objectStore = db.createObjectStore("listItems");
-  objectStore.createIndex(r
-    'item',
-    'item',
-    {unique: false}
-  );
+  var objectStore = db.createObjectStore("listItems", {autoIncrement : true});
+  objectStore.createIndex('item', 'item',{unique: false});
 
   // Listen for the completed transaction
   objectStore
